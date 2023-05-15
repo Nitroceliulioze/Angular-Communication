2581bc5692e967b9c92980a369e06542c5c887a6
Nitroceliulioze <aksionova.diana@gmail.com>
2023-05-15 16:20:50 +0300

    communicatin tith a child component

--- APM-Start v14/src/app/products/product-list.component.ts
+++ APM-Start v14/src/app/products/product-list.component.ts
@@ -1,43 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { CriteriaComponent } from '../shared/criteria/criteria.component';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
   pageTitle = 'Product List';
   showImage = false;
+  includeDetail: boolean = true;
+  @ViewChild(CriteriaComponent) filterComponent!: CriteriaComponent;
+  parentListFilter!: string;
 
   imageWidth = 50;
   imageMargin = 2;
-  errorMessage = ''
-
-  private _listFilter!: string;
-  get listFilter():string {
-    return this._listFilter;
-  }
-
-  set listFilter(value:string) {
-    this._listFilter = value;
-    this.performFilter(this.listFilter)
-  }
-
+  errorMessage = '';
 
   filteredProducts: IProduct[] = [];
   products: IProduct[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
+  ngAfterViewInit(): void {
+    this.parentListFilter = this.filterComponent.listFilter;
+  }
+  
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
-      next: products => {
+      next: (products) => {
         this.products = products;
-        this.performFilter(this.listFilter);
+        this.performFilter(this.parentListFilter);
       },
-      error: err => this.errorMessage = err
+      error: (err) => (this.errorMessage = err),
     });
   }
 
@@ -47,8 +44,12 @@ export class ProductListComponent implements OnInit {
 
   performFilter(filterBy?: string): void {
     if (filterBy) {
-      this.filteredProducts = this.products.filter(product =>
-        product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1);
+      this.filteredProducts = this.products.filter(
+        (product) =>
+          product.productName
+            .toLocaleLowerCase()
+            .indexOf(filterBy.toLocaleLowerCase()) !== -1
+      );
     } else {
       this.filteredProducts = this.products;
     }
